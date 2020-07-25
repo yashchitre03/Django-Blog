99763ed561cf749765b66d29d20de423c13df8a2
yashchitre03 <41328573+yashchitre03@users.noreply.github.com>
2020-07-24 21:52:04 -0500

    Add home and about views

--- DjangoProject/blog/views.py
+++ DjangoProject/blog/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse("<h1>Welcome to the home page</h1>")
+
+def about(request):
+    return HttpResponse("<h1> About this blog</h1><p>Hello World!</p>")
