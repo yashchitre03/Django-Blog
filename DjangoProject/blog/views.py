f423fbdfc9ad184f93296f485bb8018d90a960f5
yashchitre03 <41328573+yashchitre03@users.noreply.github.com>
2020-07-26 23:44:33 -0500

    Create Django Templates

--- DjangoProject/blog/views.py
+++ DjangoProject/blog/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+posts = [
+    {
+        'author': 'Yash',
+        'title': 'My first post',
+        'content': 'Hello, world!',
+        'date_posted': '26 July, 2020',
+    },
+    {
+        'author': 'Corey',
+        'title': 'last post',
+        'content': 'Bye, bye world!',
+        'date_posted': '28 August, 2018',
+    }
+]
+
+
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'custom'})
