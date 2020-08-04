f9a0c10851c04c4733dbe5aa883ce5f032a7fd65
yashchitre03 <41328573+yashchitre03@users.noreply.github.com>
2020-08-03 22:57:58 -0500

    Create login/logout pages

--- DjangoProject/blog/views.py
+++ DjangoProject/blog/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Post
 
 # Create your views here.
+
+@login_required
 def home(request):
     context = {
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
+@login_required
 def about(request):
     return render(request, 'blog/about.html', {'title': 'custom'})
