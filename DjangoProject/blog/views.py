60f3c4967cc870cef1c42d4320aca873a9b5fa6e
yashchitre03 <41328573+yashchitre03@users.noreply.github.com>
2020-08-06 22:01:06 -0500

    Create class views

--- DjangoProject/blog/views.py
+++ DjangoProject/blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import (ListView,
     DetailView,
-    CreateView)
+    CreateView,
+    UpdateView,
+    DeleteView)
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # Create your views here.
 
@@ -24,9 +27,41 @@ class PostDetailView(DetailView):
     model = Post
 
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 
 def about(request):
