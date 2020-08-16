2dcf6ae34f019a09b8658d8521c7c92d35b079bf
yashchitre03 <41328573+yashchitre03@users.noreply.github.com>
2020-08-15 20:00:19 -0500

    Add Post tags

--- DjangoProject/blog/models.py
+++ DjangoProject/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 # Create your models here.
 class Post(models.Model):
@@ -8,6 +9,9 @@ class Post(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    tags = TaggableManager()
+    TAGGIT_CASE_INSENSITIVE = True
 
 
     def __str__(self):
--- DjangoProject/blog/views.py
+++ DjangoProject/blog/views.py
@@ -67,7 +67,7 @@ class PostDetailView(DetailView):
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
-    fields = ['title', 'content']
+    fields = ['title', 'content', 'tags',]
 
     def form_valid(self, form):
         form.instance.author = self.request.user
@@ -76,7 +76,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
-    fields = ['title', 'content']
+    fields = ['title', 'content', 'tags',]
 
     def form_valid(self, form):
         form.instance.author = self.request.user
