11a4e9d2635c46f06789f8fc50fb165849f83ecb
yashchitre03 <41328573+yashchitre03@users.noreply.github.com>
2020-09-09 18:11:26 -0500

    Add account verification

---
+++ DjangoProject/users/tokens.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+
+
+class TokenGenerator(PasswordResetTokenGenerator):
+    pass
+
+
+account_activation_token = TokenGenerator()
--- DjangoProject/users/views.py
+++ DjangoProject/users/views.py
@@ -2,16 +2,39 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes, force_text
+from django.core.mail import EmailMessage
+from django.http import HttpResponse
+from .tokens import account_activation_token
+from django.contrib.auth import get_user_model
+
 
 # Create your views here.
+
+
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'{username}, you have been registered')
-            return redirect('login')
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            mail_subject = 'Activate your account.'
+            message = render_to_string('users/activate_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(mail_subject, message, to=[to_email])
+            email.send()
+
+            return render(request, 'users/confirm_email.html')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
@@ -21,7 +44,8 @@ def register(request):
 def profile(request):
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        p_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile)
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
@@ -37,3 +61,19 @@ def profile(request):
     }
 
     return render(request, 'users/profile.html', context)
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = get_user_model().objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, get_user_model().DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, f'{user.username}, you have been registered')
+        return redirect('login')
+    else:
+        messages.error(request, 'The activation link is invalid!')
+        return render(request, 'uesrs/confirm_email.html')
