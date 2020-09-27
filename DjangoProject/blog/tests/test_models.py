ba6043c12ec44712db6bca0a4689eab927ccc0e1
yashchitre03 <41328573+yashchitre03@users.noreply.github.com>
2020-09-27 13:57:48 -0500

    Add Comment model tests

--- DjangoProject/blog/tests/test_models.py
+++ DjangoProject/blog/tests/test_models.py
@@ -1,42 +1,81 @@
 from django.test import TestCase
-from blog.models import Post
+from blog.models import Post, Comment, Like
 from django.contrib.auth import get_user_model
+
+
+def createTestAuthor(username, email, password):
+    return get_user_model().objects.create_user(username=username,
+                                                email=email,
+                                                password=password)
+
+
+def createTestPost(username, email, password, title='Hello, World!', content='Hello, this is the content for my test case.'):
+    author = createTestAuthor(
+        username=username, email=email, password=password)
+    if author is None:
+        raise TypeError('User Model object was not created.')
+
+    return Post.objects.create(title=title,
+                               content=content,
+                               author=author)
 
 
 class PostModelTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        Post.objects.create(title='Hello, World!',
-                            content='Hello, this is the content for my test case.',
-                            author=get_user_model().objects.create_user(username='testUser',
-                                                                        email='testing@django.models',
-                                                                        password='test123'))
+        post = createTestPost(
+            username='user1', email='user1@django.app', password='testPassword')
+        if post is None:
+            raise TypeError('Post Model object was not created.')
 
-    def test_title_max_length(self):
+    def testTitleMaxLength(self):
         post = Post.objects.get(id=1)
         max_length = post._meta.get_field('title').max_length
         self.assertEquals(max_length, 100)
 
-    def test_date_posted_auto_now_add(self):
+    def testDatePostedAutoNowAdd(self):
         post = Post.objects.get(id=1)
         auto_now_add = post._meta.get_field('date_posted').auto_now_add
         self.assertTrue(auto_now_add)
 
-    def test_author_label(self):
+    def testAuthorLabel(self):
         post = Post.objects.get(id=1)
         title = post._meta.get_field('author').verbose_name
         self.assertEquals(title, 'author')
 
-    def test_view_count_default(self):
+    def testViewCountDefault(self):
         post = Post.objects.get(id=1)
         view_count = post._meta.get_field('view_count').default
         self.assertEquals(view_count, 0)
 
-    def test_object_name(self):
+    def testObjectName(self):
         post = Post.objects.get(id=1)
         self.assertEquals(str(post), 'Hello, World!')
 
-    def test_get_absolute_url(self):
+    def testGetAbsoluteUrl(self):
         post = Post.objects.get(id=1)
         self.assertEquals(post.get_absolute_url(), '/post/1/')
+
+
+class CommentModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        post = createTestPost(
+            username='user2', email='user2@django.app', password='testpassword2')
+        if post is None:
+            raise TypeError('Post Model object was not created.')
+
+        author = createTestAuthor(
+            username='user3', email='user3@django.app', password='testpassword3')
+        if author is None:
+            raise TypeError('User Model object was not created.')
+
+        Comment.objects.create(post=post, author=author,
+                               content='This is a test comment on a user post.')
+
+    def testContentMaxLength(self):
+        comment = Comment.objects.select_related('post').get(id=1)
+        max_length = comment._meta.get_field('content').max_length
+        self.assertEquals(max_length, 256)
